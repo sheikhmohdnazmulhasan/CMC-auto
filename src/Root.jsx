033d7883d40bc852b205694ff5633f357bfea244
mdnazmul42726 <nazmulofficial@outlook.com>
2023-12-30 21:29:27 +0600

    navbar Added

---
+++ src/Root.jsx
@@ -0,0 +1,43 @@
+import { Navbar, TextInput } from "keep-react";
+import { FacebookLogo, InstagramLogo, MagnifyingGlass, TwitterLogo } from "phosphor-react";
+import { NavLink } from "react-router-dom";
+import logo from '../src/assets/logo.png';
+
+const Root = () => {
+
+    const navLinks = <>
+        <NavLink>NavLink</NavLink>
+        <NavLink>NavLink</NavLink>
+        <NavLink>NavLink</NavLink>
+        <NavLink>NavLink</NavLink>
+
+    </>
+
+    return (
+        <Navbar fluid={true} className="shadow">
+            <Navbar.Container className="flex items-center justify-between">
+                <Navbar.Brand> <img src={logo} alt="CMC Logo" width="60" /> </Navbar.Brand>
+
+                <Navbar.Container className="flex items-center gap-6">
+                    <Navbar.Container tag="ul" className="lg:flex hidden items-center justify-between gap-4">
+                        <Navbar.Link icon={<FacebookLogo size={20} color="#444" />} iconAnimation={false} />
+                        <Navbar.Link icon={<InstagramLogo size={20} color="#444" />} iconAnimation={false} />
+                        <Navbar.Link icon={<TwitterLogo size={20} color="#444" />} iconAnimation={false} />
+                    </Navbar.Container>
+
+                    <Navbar.Container className="flex gap-1">
+                        <Navbar.Toggle className=" block" /> <div className="hidden md:block space-x-5">{navLinks}</div>
+                    </Navbar.Container>
+
+                </Navbar.Container>
+
+                <Navbar.Collapse collapseType="sidebar" className="fixed right-0 top-0 bg-white p-10 lg:!w-2/6 xl:!w-1/6 md:!w-2/6 w-1/2">
+                    <Navbar.Container tag="ul" className="flex flex-col gap-5">{navLinks}</Navbar.Container>
+                </Navbar.Collapse>
+            </Navbar.Container>
+        </Navbar>
+    );
+}
+
+
+export default Root;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
-/** @type {import('tailwindcss').Config} */
+import keepPreset from "keep-react/preset";
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/keep-react/**/*.{js,jsx,ts,tsx}"
+  ],
+  presets: [keepPreset],
+};
