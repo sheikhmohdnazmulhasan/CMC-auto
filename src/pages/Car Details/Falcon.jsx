3d7f4646a6b7cf853e3b38843acdd44dd1b16831
mdnazmul42726 <nazmulofficial@outlook.com>
2024-01-02 18:32:49 +0600

    updated falcon pages card image

--- src/pages/Car Details/Falcon.jsx
+++ src/pages/Car Details/Falcon.jsx
@@ -3,6 +3,11 @@ import img2 from '../../assets/Car details/3/Falcon6 (5).jpg';
 import img3 from '../../assets/Car details/3/Falcon6 (8).jpg';
 import img4 from '../../assets/Car details/3/Falcon6 (9).jpg';
 import DataCard from '../../components/DataCard';
+import imgX1 from '../../assets/Car-x/falcon/Personalization Excellence.jpg';
+import imgX2 from '../../assets/Car-x/falcon/Precision Craftsmanship.jpg';
+import imgX3 from '../../assets/Car-x/falcon/Customization Excellence.jpg';
+import imgX4 from '../../assets/Car-x/falcon/High-Tech Interior.jpg';
+
 const Falcon = () => {
     return (
         <div className="mt-20" data-aos="fade-left">
@@ -35,13 +40,13 @@ const Falcon = () => {
             <div className="mx-5 mt-20 md:mx-10">
                 <h1 className="text-3xl font-bold mb-5">Why Choose Our Falcon Car Decor Service?</h1>
                 <div className="mt-20 grid grid-cols-1 md:grid-cols-2 gap-4">
-                    <DataCard img={imgX5} title={'Personalization Excellence'} description={"Transform your Falcon with our dynamic car decoration service. Our expert designers infuse performance-inspired elements, ensuring your Falcon's interior complements its powerful nature with a touch of style"} />
+                    <DataCard img={imgX1} title={'Personalization Excellence'} description={"Transform your Falcon with our dynamic car decoration service. Our expert designers infuse performance-inspired elements, ensuring your Falcon's interior complements its powerful nature with a touch of style"} />
 
-                    <DataCard img={imgX1} title={'Precision Craftsmanship'} description={"At CMC, precision is our hallmark. Every detail is meticulously crafted to achieve a flawless finish. From cutting-edge materials to innovative design, experience the epitome of quality in every aspect of your Falcon's interior."} />
+                    <DataCard img={imgX2} title={'Precision Craftsmanship'} description={"At CMC, precision is our hallmark. Every detail is meticulously crafted to achieve a flawless finish. From cutting-edge materials to innovative design, experience the epitome of quality in every aspect of your Falcon's interior."} />
 
-                    <DataCard img={imgX7} title={'Customization Excellence'} description={"Tailor your Falcon's interior to your preferences. Our customization options range from carbon fiber accents to sporty color schemes, allowing you to create a driving environment that resonates with your individual taste."} />
+                    <DataCard img={imgX3} title={'Customization Excellence'} description={"Tailor your Falcon's interior to your preferences. Our customization options range from carbon fiber accents to sporty color schemes, allowing you to create a driving environment that resonates with your individual taste."} />
 
-                    <DataCard img={imgX3} title={'High-Tech Interior'} description={"Immerse yourself in technology with our Falcon car decor. Enjoy the latest audio-visual systems and smart connectivity features, enhancing your driving experience with the best in modern innovation."} />
+                    <DataCard img={imgX4} title={'High-Tech Interior'} description={"Immerse yourself in technology with our Falcon car decor. Enjoy the latest audio-visual systems and smart connectivity features, enhancing your driving experience with the best in modern innovation."} />
                 </div>
             </div>
         </div>
