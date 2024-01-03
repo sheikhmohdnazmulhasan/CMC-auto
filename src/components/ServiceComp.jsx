605d69e56aa961416db29a5a378fd67e339a1112
mdnazmul42726 <nazmulofficial@outlook.com>
2024-01-03 22:03:54 +0600

    updated our services pages

--- src/components/ServiceComp.jsx
+++ src/components/ServiceComp.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 const ServiceComp = ({ title, text, img, link }) => {
     return (
         <div>
-            <div className="border rounded hover:scale-105 transition-all md:w-72">
+            <div className="border rounded hover:scale-105 transition-all md:w-72 h-[450px]">
                 <figure><img className="" src={img} /></figure>
                 <div className="card-body">
                     <h2 className="card-title">{title}</h2>
-                    <p>{text}</p>
+                    <p className="text-sm">{text}</p>
                     <div className="card-actions justify-end">
                         <Link to={link}> <button className=" px-4 rounded text-white py-2 bg-[#3EA354] hover:bg-[#2f773f]">Details</button></Link>
                     </div>
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ServiceComp from '../components/ServiceComp';
 import img1 from '../assets/service/4-768x575.webp';
-
+import img2 from '../assets/service/Car-Upholstery-1067x800.webp';
+import img3 from '../assets/service/Smart-TV-Apple-TV_03.webp';
 const Services = () => {
     return (
         <div className=''>
@@ -17,10 +18,13 @@ const Services = () => {
             </div>
             <div className="mx-5 md:mx-10 mt-20">
                 <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
-                    <ServiceComp text={'Modifying the seats to provide more comfort or support, or to fit the customer’s specific body shape or size.'} title={"Seat modifications"} img={img1} />
-                    <ServiceComp text={'Modifying the seats to provide more comfort or support, or to fit the customer’s specific body shape or size.'} title={"Seat modifications"} img={img1} />
-                    <ServiceComp text={'Modifying the seats to provide more comfort or support, or to fit the customer’s specific body shape or size.'} title={"Seat modifications"} img={img1} />
-                    <ServiceComp text={'Modifying the seats to provide more comfort or support, or to fit the customer’s specific body shape or size.'} title={"Seat modifications"} img={img1} />
+                    <ServiceComp text={'Modifying the seats to provide more comfort or support, or to fit the customer’s specific body shape or size.'} title={"Seat modifications"} img={img1} link={'/seat-modification'} />
+
+                    <ServiceComp text={'Creating unique upholstery designs that perfectly match the customer’s vision, using high-quality materials such as leather, suede, or Alcantara.'} title={"Car Upholstery Service"} img={img2} link={'/auto-upholstery'} />
+
+                    <ServiceComp text={'Installing custom audio and entertainment systems that integrate seamlessly with the car’s interior design and provide '} title={"Entertainment systems"} img={img3} link={'/entertainment'} />
+
+                    <ServiceComp text={'Designing and building custom dashboards and consoles that provide a unique look and feel and incorporate advanced '} title={"Central console design"} img={img3} link={'/Central console design'} />
                 </div>
             </div>
         </div>
