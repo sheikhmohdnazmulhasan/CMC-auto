a8977c866d123b70a1e7f72b446742ee1122fb4d
mdnazmul42726 <nazmulofficial@outlook.com>
2024-01-06 00:29:34 +0600

    Elevating Luxury with Mercedes Sprinter Interior Modifications updated

--- src/pages/Car Details/Sprinter.jsx
+++ src/pages/Car Details/Sprinter.jsx
@@ -1,9 +1,23 @@
 import CarDetailsParent from '../../components/CarDetailsParent';
 
+// C
+import img1 from '../../assets/new Detail page/sprinter/carousel/12 Best Mercedes Sprinter Van Conversions On Planet Earth.jpeg';
+import img2 from '../../assets/new Detail page/sprinter/carousel/2014 Refreshed Sprinter Van Is Ready to Take All Comers _ Cars_com.jpeg';
+import img3 from '../../assets/new Detail page/sprinter/carousel/Mercedes-Benz Luxury Sprinter JetVan Conversion Vehicle _ Becker Automotive Design.jpeg';
+import img4 from '../../assets/new Detail page/sprinter/carousel/VIP Sprinter 170_ - Design 3.jpeg';
+
+// G
+import g1 from '../../assets/new Detail page/sprinter/gallery/2022 Mercedes Benz Sprinter In Delran, New Jersey, United States For Sale (12235935).jpeg';
+import g2 from '../../assets/new Detail page/sprinter/gallery/2023 Custom Mercedes Sprinter VIP - Luxury Business in Detail.jpeg';
+import g3 from '../../assets/new Detail page/sprinter/gallery/AWD Mercedes Sprinter - 2 Berth Savage - Mclaren Sports Homes Ltd _ Luxury Sporthome & Motorhome Conversions.jpeg';
+import g4 from '../../assets/new Detail page/sprinter/gallery/Mercedes-Benz Luxury Sprinter JetVan Conversion Vehicle _ Becker Automotive Design.jpeg';
+import g5 from '../../assets/new Detail page/sprinter/gallery/sportsmobile-four-by-four-09.jpg';
+import g6 from '../../assets/new Detail page/sprinter/gallery/VIP Sprinter 170_ - Design 3.jpeg';
+
 const Sprinter = () => {
     return (
         <div>
-            <CarDetailsParent />
+            <CarDetailsParent img1={img1} img2={img2} img3={img3} img4={img4} title={"Elevating Luxury with Mercedes Sprinter Interior Modifications"} des={"At CMC Automobile Outfittings, we excel in elevating the ordinary to extraordinary. Redefining luxury is our passion, showcased in our Mercedes Sprinter interior modifications. They embody peak sophistication, comfort, and style, setting new standards in automotive opulence."} li1={"Personalized Luxury: Tailored interior modifications that reflect your style, maximizing comfort in your Mercedes Sprinter."} li2={"Precision Craftsmanship: Meticulous attention to detail ensures superior quality and perfection in every modification."} li3={"Tech-Forward: Integration of cutting-edge technology for enhanced convenience and entertainment."} li4={"Opulent Comfort: Premium materials and ergonomic design prioritize comfort, making each journey a luxurious experience."} vTitle={"We believe that true luxury lies in the finest details"} vDes={"That’s why we source materials of the highest caliber to adorn your Mercedes Sprinter. Sumptuous leather, exotic woods, and other premium finishes are meticulously selected to ensure an aura of sophistication and refinement."} bTitle={"Ultimate Luxury: Mercedes Sprinter Interior Transformations by CMC"} bDes1={"Long journeys should be embraced, not endured. Our modifications prioritize comfort and functionality, ensuring that every ride in your Mercedes Sprinter is a luxurious and effortless experience. Whether it's reclining seats, enhanced storage solutions, or innovative design, your comfort is our priority."} bDes2={"Step into a world where luxury knows no bounds. With CMC Automobile Outfittings, your Mercedes Sprinter transcends its original form to become a masterpiece of luxury and elegance. Embrace the extraordinary and redefine your driving experience with our bespoke interior modifications."} g1={g1} g2={g2} g3={g3} g4={g4} g5={g5} g6={g6} />
         </div>
     );
 };
