1828ffe1f39c5a4f9028d5a92b0e386fb49ef80c
mdnazmul42726 <nazmulofficial@outlook.com>
2024-01-06 00:19:02 +0600

    Unveiling the Falcon VIP Experience: Redefining Luxury Travel updated

--- src/Root.jsx
+++ src/Root.jsx
@@ -63,7 +63,7 @@ const Root = () => {
                 <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
                 <div className="drawer-content flex flex-col">
                     {/* Navbar */}
-                    <Headroom className=" bg-white"> <div className={hideTopNavbar ? 'w-full navbar bg-white' : 'w-full navbar bg-white border-b'}>
+                    <Headroom className=" bg-white z-[50000000000]"> <div className={hideTopNavbar ? 'w-full navbar bg-white' : 'w-full navbar bg-white border-b'}>
                         <div className="flex-none lg:hidden" onClick={() => setHideTopNavbar(!hideTopNavbar)}>
                             <label htmlFor="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
                                 {hideTopNavbar ? <MdOutlineCancel className="text-2xl" /> : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>}
--- src/pages/Car Details/Falcon.jsx
+++ src/pages/Car Details/Falcon.jsx
@@ -1,9 +1,23 @@
 import CarDetailsParent from "../../components/CarDetailsParent";
 
+// C
+import img1 from '../../assets/new Detail page/falcon/carousel/1.jpg';
+import img2 from '../../assets/new Detail page/falcon/carousel/2.jpg';
+import img3 from '../../assets/new Detail page/falcon/carousel/3.jpg';
+import img4 from '../../assets/new Detail page/falcon/carousel/4.jpg';
+
+// G
+import g1 from '../../assets/new Detail page/falcon/gallery/IMG-20231214-WA0207.jpg';
+import g2 from '../../assets/new Detail page/falcon/gallery/IMG-20231214-WA0208.jpg';
+import g3 from '../../assets/new Detail page/falcon/gallery/IMG-20231214-WA0209.jpg';
+import g4 from '../../assets/new Detail page/falcon/gallery/IMG-20231214-WA0211.jpg';
+import g5 from '../../assets/new Detail page/falcon/gallery/IMG-20231214-WA0212.jpg';
+import g6 from '../../assets/new Detail page/falcon/gallery/IMG-20231214-WA0213.jpg';
+
 const Falcon = () => {
     return (
         <div>
-            <CarDetailsParent />
+            <CarDetailsParent img1={img1} img2={img2} img3={img3} img4={img4} title={"Unveiling the Falcon VIP Experience: Redefining Luxury Travel"} des={"Introducing the Falcon VIP Experience, a pinnacle of luxury within the Falcon series. Crafted to seamlessly merge opulence with cutting-edge innovation, Falcon Motors brings you an unmatched journey within the Falcon car, combining meticulous design, premium materials, and state-of-the-art technology. Elevate every moment of your travel with our bespoke, luxury interior solutions, specially tailored for the Falcon. Embark on a transformative journey where luxury meets innovation, meticulously redefining comfort and opulence within the Falcon."} li1={"Signature Edition: Exquisite refinement paired with unparalleled comfort."} li2={"Prestige Edition: Elevating your travels with unmatched elegance and luxurious amenities."} li3={"Executive Suite: Tailored for business elites, seamlessly fusing functionality with opulence."} li4={"Corporate Class: Redefining sophistication for professionals, blending style with practicality."} vTitle={"Immerse yourself in the epitome of luxury with Falcon Motors"} vDes={"Your premier destination for the Falcon VIP Experience. Our dedication shines through in transforming the Falcon into extraordinary editions: Signature, Prestige, Executive Suite, and Corporate Class. Each edition represents the pinnacle of elegance, adaptability, and flawless craftsmanship, setting a new benchmark for sophistication on wheels."} bTitle={"Crafting Elegance: Inside the Falcon VIP Series by CMC AUTOMOBILE."} bDes1={"Explore an expanded realm of luxury with the Falcon Luxury Series by Falcon Motors. Beyond the renowned editions, our lineup includes the Falcon GT, Falcon LX, and Falcon Elite, catering to diverse preferences and needs."} bDes2={"Whether you seek the zenith of opulence, a mobile workspace that enhances productivity, or presidential-level travel, our meticulously crafted Falcon VIP lineup exceeds expectations. Embrace exclusivity and comfort with our esteemed range. Contact us today to step into a world defined by luxury, sophistication, and uncompromising excellence. Your personalized Falcon VIP Experience awaits, poised to redefine your journey to unparalleled heights."} g1={g1} g2={g2} g3={g3} g4={g4} g5={g5} g6={g6} />
         </div>
     );
 };
