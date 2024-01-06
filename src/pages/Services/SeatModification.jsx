7ea99dca887c7a7289ee84999c7607eea1dcd101
mdnazmul42726 <nazmulofficial@outlook.com>
2024-01-07 00:29:29 +0600

    Seat Modification page updated

--- src/pages/Services/SeatModification.jsx
+++ src/pages/Services/SeatModification.jsx
@@ -4,10 +4,11 @@ import s3 from '../../assets/Seat_6.webp';
 import s4 from '../../assets/Seat_7.webp';
 import s5 from '../../assets/Seat_8.webp';
 import s6 from '../../assets/Seat_1.webp';
+import { Faq } from '../../components/Faq';
 
 const SeatModification = () => {
     return (
-        <div className="">
+        <div className="bg-[url('https://i.ibb.co/NshGgLF/CMC-WEB-BG.jpg')] bg-cover text-white" >
 
             {/* Banner */}
             <div className="bg-[url('https://th.bing.com/th/id/R.c676fc057467fc822a6b18c06e0f5048?rik=UAp8PRAp0407GA&riu=http%3a%2f%2f3.bp.blogspot.com%2f_3Nq7CKYaRdQ%2fS8sWBuGPhFI%2fAAAAAAAANEw%2fuoSspVEI--E%2fs1600%2fporsche-techart-black-edition-panamera-5.jpg&ehk=TbhVNRZVwiAamSpQ3zQIKeI2w0Wek7CQhYapIp%2bIGRk%3d&risl=&pid=ImgRaw&r=0')] h-72 flex justify-center items-center text-white bg-fixed bg-cover">
@@ -15,9 +16,9 @@ const SeatModification = () => {
             </div>
 
             {/* Main Content */}
-            <div className="m-10">
-                <h3 className='text-3xl font-semibold text-slate-500'>Seat Modification</h3>
-                <div className="mt-10 opacity-65 space-y-4 text-sm">
+            <div className="px-5 md:px-10">
+                <h3 className='text-3xl font-semibold'>Seat Modification</h3>
+                <div className="mt-10 opacity-80 space-y-4 text-sm">
                     <p>Welcome to CMC, where luxury and comfort are redefined to meet the most discerning tastes. Our expertise lies in transforming the interior of your Mercedes-Benz or other top-tier brands into an oasis of sophistication and relaxation. Discover how we can elevate your driving experience with our premium seat modification services:</p>
 
                     <p> <span className='font-semibold'>VIP Seats with Massage System (8 Airbags):</span> Experience the epitome of comfort with our VIP seats, meticulously crafted to provide you with the utmost relaxation during your journeys. Our massage system, featuring 8 airbags, is designed to target key pressure points, easing tension and promoting overall well-being. Let the stress of the road melt away as you indulge in a therapeutic massage while driving or sitting in your vehicle.</p>
@@ -33,60 +34,21 @@ const SeatModification = () => {
 
                 {/* Images */}
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-4 md:ml-5 my-10">
-                    <img className='md:w-[80%] rounded hover:scale-105 transition-all' src={s1} alt="" />
-                    <img className='md:w-[80%] rounded hover:scale-105 transition-all' src={s2} alt="" />
-                    <img className='md:w-[80%] rounded hover:scale-105 transition-all' src={s3} alt="" />
-                    <img className='md:w-[80%] rounded hover:scale-105 transition-all' src={s4} alt="" />
-                    <img className='md:w-[80%] rounded hover:scale-105 transition-all' src={s5} alt="" />
-                    <img className='md:w-[80%] rounded hover:scale-105 transition-all' src={s6} alt="" />
+                    <img className=' rounded hover:scale-105 transition-all' src={s1} alt="" />
+                    <img className=' rounded hover:scale-105 transition-all' src={s2} alt="" />
+                    <img className=' rounded hover:scale-105 transition-all' src={s3} alt="" />
+                    <img className=' rounded hover:scale-105 transition-all' src={s4} alt="" />
+                    <img className=' rounded hover:scale-105 transition-all' src={s5} alt="" />
+                    <img className=' rounded hover:scale-105 transition-all' src={s6} alt="" />
                 </div>
 
-                <div className=" opacity-65 space-y-4 text-sm">
+                <div className=" opacity-80 space-y-4 text-sm">
                     <p>At CMC, we understand that the interior of your vehicle is a reflection of your taste and style. Our seat modification services are designed to cater to the most discerning automotive enthusiasts, ensuring that every moment you spend inside your vehicle is a testament to luxury and comfort.</p>
 
                     <p>Experience the pinnacle of seat modification with Scuderia Motor Design. Contact us today to schedule a consultation and embark on a journey to transform your vehicle’s interior into a haven of luxury and relaxation. Your destination is comfort, and it starts here with us.</p>
                 </div>
-
-                {/* Accordion */}
-                <div className="w-full my-16 space-y-4">
-                    <h3 className='text-2xl'>Why Choose CMC?</h3>
-                    <div className="collapse collapse-arrow bg-base-200 opacity-80">
-                        <input type="radio" name="my-accordion-2" checked="checked" />
-                        <div className="collapse-title font-medium">
-                            Custom Seat Design and Installation
-                        </div>
-                        <div className="collapse-content">
-                            <p>Our team of experienced professionals will work closely with you to create custom seat designs that suit your specific preferences and requirements. From ergonomic designs to luxurious materials, we ensure that every seat is tailored to perfection. We handle the entire installation process with precision and expertise, guaranteeing a seamless integration into your vehicle.</p>
-                        </div>
-                    </div>
-                    <div className="collapse collapse-arrow bg-base-200 opacity-80">
-                        <input type="radio" name="my-accordion-2" />
-                        <div className="collapse-title font-medium">
-                            Custom Seat Design and Installation
-                        </div>
-                        <div className="collapse-content">
-                            <p>Our team of experienced professionals will work closely with you to create custom seat designs that suit your specific preferences and requirements. From ergonomic designs to luxurious materials, we ensure that every seat is tailored to perfection. We handle the entire installation process with precision and expertise, guaranteeing a seamless integration into your vehicle.</p>
-                        </div>
-                    </div>
-                    <div className="collapse collapse-arrow bg-base-200 opacity-80">
-                        <input type="radio" name="my-accordion-2" />
-                        <div className="collapse-title font-medium">
-                            Seat Heating and Cooling Solutions
-                        </div>
-                        <div className="collapse-content">
-                            <p>Experience ultimate comfort regardless of the weather with our seat heating and cooling solutions. We integrate advanced temperature control systems into your seats, allowing you to adjust the climate according to your preference. Say goodbye to uncomfortable seats during extreme weather conditions and enjoy a pleasant journey all year round.</p>
-                        </div>
-                    </div>
-                    <div className="collapse collapse-arrow bg-base-200 opacity-80">
-                        <input type="radio" name="my-accordion-2" />
-                        <div className="collapse-title font-medium">
-                            Lumbar Support and Ergonomic Enhancements
-                        </div>
-                        <div className="collapse-content">
-                            <p>If you suffer from back pain or discomfort during long drives, our lumbar support and ergonomic enhancements are the perfect solution. We utilize cutting-edge technology and design principles to provide optimal support to your back, reducing strain and promoting better posture. Enjoy a pain-free driving experience and arrive at your destination feeling refreshed and rejuvenated.</p>
-                        </div>
-                    </div>
-
+                <div className="mt-20 mx-5 md:mx-10">
+                    <Faq title1={" Expert Craftsmanship"} des1={"Our team of highly skilled upholsterers has years of experience in the industry. We take pride in our meticulous attention to detail and commitment to delivering exceptional results."} title2={"  Customization Options"} des2={"We understand that every client has unique preferences and requirements. That’s why we offer a wide range of customization options, including fabric selection, color schemes, patterns, stitching styles, and additional features. Our goal is to create a tailored interior that reflects your personal style and meets your specific needs"} title3={"  Premium Materials"} des3={"We source only the finest materials for our upholstery projects. From luxurious fabrics to durable and easy-to-clean options, we ensure that your van’s interior is not only visually stunning but also built to withstand the demands of your lifestyle"} title4={"  Timely Delivery"} des4={"We understand the excitement of transforming your van, and we strive to complete projects within the agreed-upon timeline. Our efficient processes and dedicated team enable us to deliver on time without compromising on quality"} />
                 </div>
             </div>
         </div>
