6115fac68d2bd4b82d0346c5c0e2c2d0a5d29e39
mdnazmul42726 <nazmulofficial@outlook.com>
2024-01-07 00:34:50 +0600

    Car Upholstery Service page updated

--- src/pages/Services/CarUpholstery.jsx
+++ src/pages/Services/CarUpholstery.jsx
@@ -1,19 +1,20 @@
 import fabric from '../../assets/Fabric.webp';
 import leather from '../../assets/Leather.webp';
 import croCstorage from '../../assets/CroCstorage.webp';
+import { Faq } from '../../components/Faq';
 
 const CarUpholstery = () => {
     return (
-        <div className="">
+        <div className="bg-[url('https://i.ibb.co/NshGgLF/CMC-WEB-BG.jpg')] bg-cover text-white" >
             {/* Banner */}
             <div className="bg-[url('https://fantasticservicesgroup.com.au/blog/wp-content/uploads/2021/05/how-to-clean-car-upholstery_header.jpg')] h-72 flex justify-center items-center text-white bg-fixed bg-cover">
                 <h3 className='text-4xl font-bold'>Car Upholstery Service</h3>
             </div>
 
             {/* Main Content */}
-            <div className="m-10">
-                <h3 className='text-3xl font-semibold text-slate-500'>Auto Upholstery Specialist </h3>
-                <div className="mt-10 opacity-65 space-y-4 text-sm">
+            <div className="px-5 md:px-10">
+                <h3 className='text-3xl font-semibold mt-4'>Auto Upholstery Specialist </h3>
+                <div className="mt-10 opacity-80 space-y-4 text-sm">
                     <p>Welcome to CMC – Your Premier Auto Upholstery Specialists</p>
 
                     <p>At CMC, we’re more than just experts in car upholstery; we are passionate craftsmen dedicated to elevating your driving experience. Our key services encompass a world of luxury, comfort, and style, all centered around your vehicle’s interior. Explore how our Auto Upholstery Specialists can transform your ride:</p>
@@ -23,14 +24,14 @@ const CarUpholstery = () => {
             </div>
 
             {/* Images */}
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-4 md:ml-5 my-10 mx-4 w-full">
-                <img className='md:w-[75%] rounded hover:scale-105 transition-all' src={leather} alt="" />
-                <img className='md:w-[75%] rounded hover:scale-105 transition-all' src={fabric} alt="" />
-                <img className='md:w-[75%] rounded hover:scale-105 transition-all' src={croCstorage} alt="" />
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4 md:ml-5 my-10 px-10 w-full">
+                <img className=' rounded hover:scale-105 transition-all' src={leather} alt="" />
+                <img className=' rounded hover:scale-105 transition-all' src={fabric} alt="" />
+                <img className=' rounded hover:scale-105 transition-all' src={croCstorage} alt="" />
             </div>
             <div className="mx-6 md:mx-10">
-                <h3 className='text-2xl font-semibold text-slate-500'>Our Comprehensive Services Include: </h3>
-                <ul className='list-disc mx-10 opacity-65 space-y-4 text-sm mt-5'>
+                <h3 className='text-2xl font-semibold '>Our Comprehensive Services Include: </h3>
+                <ul className='list-disc mx-10 opacity-80 space-y-4 text-sm mt-5'>
                     <li>
                         <span className='font-semibold'>Leather Seats (Custom Design Making): </span> Crafted with precision, our custom-designed leather seats are the epitome of luxury and style.
                     </li>
@@ -70,46 +71,8 @@ const CarUpholstery = () => {
 
             </div>
             {/* Accordion */}
-            <div className="w-full my-16 space-y-4 px-6 md:px-10">
-                <h3 className='text-2xl'>Why Choose CMC?</h3>
-                <div className="collapse collapse-arrow bg-base-200 opacity-80">
-                    <input type="radio" name="my-accordion-2" checked="checked" />
-                    <div className="collapse-title font-medium">
-                        Expert Craftsmanship
-                    </div>
-                    <div className="collapse-content">
-                        <p>Our team of highly skilled upholsterers has years of experience in the industry. We take pride in our meticulous attention to detail and commitment to delivering exceptional results.</p>
-                    </div>
-                </div>
-                <div className="collapse collapse-arrow bg-base-200 opacity-80">
-                    <input type="radio" name="my-accordion-2" />
-                    <div className="collapse-title font-medium">
-                        Customization Options
-
-                    </div>
-                    <div className="collapse-content">
-                        <p>We understand that every client has unique preferences and requirements. That’s why we offer a wide range of customization options, including fabric selection, color schemes, patterns, stitching styles, and additional features. Our goal is to create a tailored interior that reflects your personal style and meets your specific needs</p>
-                    </div>
-                </div>
-                <div className="collapse collapse-arrow bg-base-200 opacity-80">
-                    <input type="radio" name="my-accordion-2" />
-                    <div className="collapse-title font-medium">
-                        Premium Materials
-                    </div>
-                    <div className="collapse-content">
-                        <p>We source only the finest materials for our upholstery projects. From luxurious fabrics to durable and easy-to-clean options, we ensure that your van’s interior is not only visually stunning but also built to withstand the demands of your lifestyle</p>
-                    </div>
-                </div>
-                <div className="collapse collapse-arrow bg-base-200 opacity-80">
-                    <input type="radio" name="my-accordion-2" />
-                    <div className="collapse-title font-medium">
-                        Timely Delivery
-                    </div>
-                    <div className="collapse-content">
-                        <p>We understand the excitement of transforming your van, and we strive to complete projects within the agreed-upon timeline. Our efficient processes and dedicated team enable us to deliver on time without compromising on quality</p>
-                    </div>
-                </div>
-
+            <div className="mt-20 mx-5 md:mx-10">
+                <Faq title1={" Expert Craftsmanship"} des1={"Our team of highly skilled upholsterers has years of experience in the industry. We take pride in our meticulous attention to detail and commitment to delivering exceptional results."} title2={"  Customization Options"} des2={"We understand that every client has unique preferences and requirements. That’s why we offer a wide range of customization options, including fabric selection, color schemes, patterns, stitching styles, and additional features. Our goal is to create a tailored interior that reflects your personal style and meets your specific needs"} title3={"  Premium Materials"} des3={"We source only the finest materials for our upholstery projects. From luxurious fabrics to durable and easy-to-clean options, we ensure that your van’s interior is not only visually stunning but also built to withstand the demands of your lifestyle"} title4={"  Timely Delivery"} des4={"We understand the excitement of transforming your van, and we strive to complete projects within the agreed-upon timeline. Our efficient processes and dedicated team enable us to deliver on time without compromising on quality"} />
             </div>
         </div>
     );
